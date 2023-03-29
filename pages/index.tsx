7523efa8dd97156f708210e190d1f161ac8c8764
Chris Park <crashpark@gmail.com>
2023-03-29 17:29:23 +1100

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,11 +12,36 @@ export default function Home() {
       <main className="container">
         <h1 className="dark-blue">Parkys First Headless Website</h1>
         <p>
-          Welcome to my first headless website! Im using Next.js to build this amazing site, and its incredibly cool. Next.js
-          offers so many fantastic features, such as server-side rendering, static site generation, and API routes. Im excited
-          to see what I can create with it.
+          Welcome to my first headless website! 
+          This site is very cool and everyone else is just jealous.
         </p>
 
+        <style jsx>{`
+          .container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #f5f5f5;
+            padding: 2rem;
+          }
+
+          h1.dark-blue {
+            font-size: 3rem;
+            font-weight: bold;
+            color: #2c3e50;
+            margin-bottom: 1rem;
+          }
+
+          p {
+            font-size: 1.5rem;
+            line-height: 1.6;
+            max-width: 800px;
+            text-align: center;
+            color: #34495e;
+          }
+        `}</style>
       </main>
     </div>
   );
